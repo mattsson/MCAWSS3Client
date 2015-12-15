16386d24796784430c97acfe1af462eaee99c3aa
Marcus Mattsson <marcus.mattsson@gmail.com>
2015-12-15 15:07:23 +0100

    Exposed initWithBaseURL

--- MCAWSS3Client.h
+++ MCAWSS3Client.h
@@ -44,6 +44,8 @@ typedef enum {
 @property (nonatomic, retain) NSString* bucket;
 @property (nonatomic, assign) BOOL integrityCheck;
 
+- (instancetype)initWithBaseURL:(NSURL*)url;
+
 - (void)putObjectWithData:(NSData*)data key:(NSString*)key mimeType:(NSString*)mimeType success:(void (^)(AFHTTPRequestOperation* operation, id responseObject))success failure:(void (^)(AFHTTPRequestOperation* operation, NSError* error))failure;
 - (void)putObjectWithData:(NSData*)data key:(NSString*)key mimeType:(NSString*)mimeType permission:(MCAWSS3ObjectPermission)permission progress:(void (^)(NSUInteger bytesWritten, long long totalBytesWritten, long long totalBytesExpectedToWrite))progress success:(void (^)(AFHTTPRequestOperation* operation, id responseObject))success failure:(void (^)(AFHTTPRequestOperation* operation, NSError* error))failure;
 
